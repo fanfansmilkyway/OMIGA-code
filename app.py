008e4a37f8697cf90a6aa18825ad293d87a2c07a
fanfansmilkyway <fanfansmilkyway@qq.com>
2023-10-27 21:01:56 +0800

    Add shortcut bar(BETA3.2)

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 # Built-in modules
 import os
 # Need to install(not built-in)
-from flask import Flask, request, render_template, redirect, session
+from flask import Flask, request, render_template, redirect, session, flash
 import duckdb
 
 # Environment Variables
@@ -15,6 +15,7 @@ app.secret_key = 'q776NkmVYq3vjZwaJn9drw'
 OMIGA_dictionary = duckdb.connect('{0}/dictionary.db'.format(DATABASE))
 discussion_database = duckdb.connect("{0}/discussion.db".format(DATABASE))
 con = duckdb.connect("{0}/userdata.db".format(DATABASE))
+
 # Create database if it not exists
 con.sql("CREATE TABLE IF NOT EXISTS users(Id int primary key, Name varchar, Password varchar, Number varchar)")
 con.sql("CREATE SEQUENCE IF NOT EXISTS seq_id START 1")
@@ -30,12 +31,16 @@ discussion_database.sql("CREATE SEQUENCE IF NOT EXISTS comment_seq_id START 1")
 def index():
     if request.method == 'POST':
         if request.form['submit_button'] == '登录':
+            session['last_page'] = '/'
             return redirect('/sign_in')
     return render_template('index.html', username=session.get('username', 'Guest'))
 
 @app.route('/words', methods=['GET', 'POST'])
 def words():
     if request.method == 'POST':
+        if request.form['submit_button'] == '登录':
+            session['last_page'] = "/words"
+            return redirect('/sign_in')
         # Choose searching mode
         if request.form['submit_button'] == '直接查找':
             return redirect('/words/search-word-with-OMIGA')
@@ -48,12 +53,17 @@ def words():
             for k in all_words:
                 text = text + k + ", "
             return text
+        
+    # Count the words in the dictionary
     rows = OMIGA_dictionary.sql("SELECT COUNT(*) FROM dictionary").fetchall()[0][0]
-    return render_template('words.html', rows=rows)
+    return render_template('words.html', rows=rows, username=session.get('username', 'Guest'))
     
 @app.route('/words/search-word-with-OMIGA', methods=['GET', 'POST'])
 def search_word_with_OMIGA():
     if request.method == 'POST':
+        if request.form['submit_button'] == '登录':
+            session['last_page'] = "/words/search-word-with-OMIGA"
+            return redirect('/sign_in')
         word = request.form['word']
         # Check if word exists
         if OMIGA_dictionary.sql("SELECT EXISTS(SELECT * FROM dictionary WHERE Word='{0}')".format(word)).fetchall()[0] == (True,):
@@ -70,11 +80,14 @@ def search_word_with_OMIGA():
         else:
             # If not, show error message
             return "<h1>未找到此单词</h1>"
-    return render_template('search-word-with-OMIGA.html')
+    return render_template('search-word-with-OMIGA.html', username=session.get('username', 'Guest'))
 
 @app.route('/words/search-word-with-meaning', methods=['GET', 'POST'])
 def search_word_with_chinese():
     if request.method == 'POST':
+        if request.form['submit_button'] == '登录':
+            session['last_page'] = "/words/search-word-with-meaning"
+            return redirect('/sign_in')
         search_expresion = request.form['search']
         possible_words = []
         # Read ALL words from dataset
@@ -94,11 +107,14 @@ def search_word_with_chinese():
         if result == '':
             # If not, show error message
             return '<h1>未找到单词'
-    return render_template('search-word-with-meaning.html')
+    return render_template('search-word-with-meaning.html', username=session.get('username', 'Guest'))
 
 @app.route('/lessons', methods=['GET','POST'])
 def lessons():
     if request.method == 'POST':
+        if request.form['submit_button'] == '登录':
+            session['last_page'] = "/lessons"
+            return redirect('/sign_in')
         # Choose lesson
         if request.form['submit_button'] == '第一课：OMIGA语言的简介与导入':
             return render_template('lessons/第一课：OMIGA语言的简介与导入.html')
@@ -112,7 +128,7 @@ def lessons():
             return render_template('lessons/第五课：dv ing sihoma tsu loyode 班级中的规则.html')
         if request.form['submit_button'] == '第六课：noku, misu, kongmi 没有，一些，很多':
             return render_template('lessons/第六课：noku, misu, kongmi 没有，一些，很多.html')
-    return render_template('lessons.html')
+    return render_template('lessons.html', username=session.get('username', 'Guest'))
 
 # Function: Convert txt file to html
 def txt_web_show(filepath):
@@ -120,11 +136,14 @@ def txt_web_show(filepath):
     with open(filepath, 'r') as f:
         content = f.read()
         f.close()
-    return render_template('passages-view.html', content = content)
-    
+    return render_template('passages-view.html', content = content, username=session.get('username', 'Guest'))
+
 @app.route('/passages', methods=['GET', 'POST'])
 def passages():
     if request.method == 'POST':
+        if request.form['submit_button'] == '登录':
+            session['last_page'] = "/passages"
+            return redirect('/sign_in')
         # Choose passage
         if request.form['submit_button'] == 'hotini hegude 校园政府':
             return redirect('/passages/hotini-hegude')
@@ -138,11 +157,14 @@ def passages():
             return txt_web_show('stories/nv-eyi两个家.txt')
         if request.form['submit_button'] == 'sayoma buno a maki天堂鸟与女孩':
             return txt_web_show('stories/sayoma-buno-a-maki天堂鸟与女孩.txt')
-    return render_template('passages.html')
+    return render_template('passages.html', username=session.get('username', 'Guest'))
 
 @app.route('/passages/hotini-hegude', methods=['GET', 'POST'])
 def hotini_hegude():
     if request.method == 'POST':
+        if request.form['submit_button'] == '登录':
+            session['last_page'] = "/passages/hotini-hegude"
+            return redirect('/sign_in')
         # Choose chapter
         if request.form['submit_button'] == '1.memode':
             return txt_web_show('hotini-hegude/yi-memode.txt')
@@ -150,11 +172,14 @@ def hotini_hegude():
             return txt_web_show('hotini-hegude/nv-honakode.txt')
         if request.form['submit_button'] == '3.goku':
             return txt_web_show('hotini-hegude/sv-goku.txt')
-    return render_template('hotini-hegude.html')
+    return render_template('hotini-hegude.html', username=session.get('username', 'Guest'))
 
 @app.route('/uploads', methods=['GET', 'POST'])
 def uploads():
     if request.method == 'POST':
+        if request.form['submit_button'] == '登录':
+            session['last_page'] = "/uploads"
+            return redirect('/sign_in')
         # Useful Function: Check if word already existed
         if request.form['submit_button'] == '检查':
             if OMIGA_dictionary.sql("SELECT EXISTS(SELECT * FROM dictionary WHERE Word='{0}')".format(request.form['check'])).fetchall()[0] == (True, ):
@@ -173,27 +198,34 @@ def uploads():
                 return '<h1>不能添加空的单词！！</h1>'
             OMIGA_dictionary.sql("INSERT INTO dictionary (Id, Word, Meaning) VALUES (nextval('seq_id'), '{0}', '{1}')".format(title, content))
             return '<h1>成功添加单词</h1>'
-    return render_template('uploads.html')
+    return render_template('uploads.html', username=session.get('username', 'Guest'))
 
 @app.route('/edit', methods=['GET', 'POST'])
 def edit():
     if request.method == 'POST':
-        word = str(request.form['word'])
-        # Check if the word exists
-        if OMIGA_dictionary.sql("SELECT EXISTS(SELECT * FROM dictionary WHERE Word='{0}')".format(word)).fetchall()[0] == (True,):
-            # If yes, redirect to /edit/editing
-            session['word'] = word
-            return redirect('/edit/editing')
-        else:
-            # If not, show error message
-            return '<h1>此单词还未存在于字典中，若想添加此单词请前往<a href="/uploads">omiga.org/uploads</a></h1>'
-    return render_template('edit.html')
+        if request.form['submit_button'] == '登录':
+            session['last_page'] = "/edit"
+            return redirect('/sign_in')
+        if request.form['submit_button'] == '编辑':
+            word = str(request.form['word'])
+            # Check if the word exists
+            if OMIGA_dictionary.sql("SELECT EXISTS(SELECT * FROM dictionary WHERE Word='{0}')".format(word)).fetchall()[0] == (True,):
+                # If yes, redirect to /edit/editing
+                session['word'] = word
+                return redirect('/edit/editing')
+            else:
+                # If not, show error message
+                return '<h1>此单词还未存在于字典中，若想添加此单词请前往<a href="/uploads">omiga.org/uploads</a></h1>'
+    return render_template('edit.html', username=session.get('username', 'Guest'))
 
 @app.route('/edit/editing', methods=['GET', 'POST'])
 def editing():
     # Read from dataset
     content = OMIGA_dictionary.sql("SELECT Meaning FROM dictionary WHERE Word='{0}'".format(session['word'])).df()['Meaning'][0]
     if request.method == 'POST':
+        if request.form['submit_button'] == '登录':
+            session['last_page'] = "/edit/editing"
+            return redirect('/sign_in')
         if request.form['submit_button'] == '完成':
             edited_content = request.form['content']
             # Check if the content is empty
@@ -205,32 +237,38 @@ def editing():
         if request.form['submit_button'] == '删除此单词':
             OMIGA_dictionary.sql("DELETE FROM dictionary WHERE Word='{0}'".format(session['word']))
             return "<h1>成功删除此单词</h1>"
-    return render_template('editing.html', word=session['word'], content=content)
+    return render_template('editing.html', word=session['word'], content=content, username=session.get('username', 'Guest'))
 
 @app.route('/discussion', methods=['GET', 'POST'])
 def discussion():
     my_list = discussion_database.sql("SELECT * EXCLUDE Content FROM topic").fetchall()
     my_list.reverse()
     if request.method == 'POST':
+        if request.form['submit_button'] == '登录':
+            session['last_page'] = "/discussion"
+            return redirect('/sign_in')
         if request.form['submit_button'] == "创作一篇文章":
             return redirect('/discussion/create')
-        if request.form['submit_button'] == '登录':
-            return redirect('/sign_in')
     return render_template('discussion.html', my_list=my_list, username=session.get('username', 'Guest'))
 
 @app.route('/discussion/create', methods=['GET', 'POST'])
 def discussion_create():
     if 'username' not in session:
+        session['last_page'] = "/discussion/create"
         return redirect('/sign_in')
     if request.method == 'POST':
         title = request.form['title']
         content = request.form['content']
         discussion_database.sql("INSERT INTO topic (Id, Title, Content, CreatedTime, Username) VALUES (nextval('topic_seq_id'), '{0}', '{1}', CURRENT_TIMESTAMP::STRING, '{2}')".format(title, content, session['username']))
+        return redirect('/discussion')
     return render_template('discussion-create.html', username=session.get('username', 'Guest'))
 
 @app.route('/discussion/view/<passage_id>', methods=['GET', 'POST'])
 def view_passage(passage_id):
     if request.method == 'POST':
+        if request.form['submit_button'] == '登录':
+            session['last_page'] = "/discussion/view/{0}".format(passage_id)
+            return redirect('/sign_in')
         if 'username' not in session:
             return redirect('/sign_in')
         comment = request.form['comment']
@@ -256,7 +294,7 @@ def sign_up():
         if con.sql("SELECT EXISTS(SELECT * FROM users WHERE Name='{0}')".format(user_name)).fetchall()[0] == (True,):
             # If not, show error message
             return '<h1>此用户名已经被使用</h1>'
-    return render_template('sign_up.html')
+    return render_template('sign_up.html', username=session.get('username', 'Guest'))
 
 @app.route('/sign_in', methods=['GET','POST'])
 def sign_in():
@@ -276,6 +314,11 @@ def sign_in():
             # Check if the password is correct
             if password == real_password:
                 session['username'] = user_name
+                if 'last_page' in session:
+                    last_page = session['last_page']
+                    session.pop('last_page', None)
+                    return redirect(last_page)
+                return '<h1>登录成功</h1>'
             else:
                 return '<h1>密码错误</h1>'
         if request.form['submit_button'] == '退出账号':
@@ -301,20 +344,20 @@ def users():
         con.sql("UPDATE users SET number={0} WHERE Name='{1}'".format(new_number, session['username']))
         return '<h1>成功更改</h1>'
 
-    return render_template('users.html', user_name=session['username'], num=number)
+    return render_template('users.html', user_name=session['username'], num=number, username=session.get('username', 'Guest'))
 
 @app.errorhandler(404)
 def page_not_found(error):
-    return "404PageNotFound: 未找到此页面,请检查你的网址是否有错误", 404
+    return render_template('404NotFound.html'), 404
 
 # Run!!!!!
 if __name__ == '__main__':
     print('''                             
    ___  __  __ ___ ____    _            
   / _ \|  \/  |_ _/ ___|  / \     
- | | | | |\/| || | |  _  / _ \      Version: BETA3.1
+ | | | | |\/| || | |  _  / _ \      Version: BETA3.2
  | |_| | |  | || | |_| |/ ___ \     Contributor: Fanfansmilyway, Fkpwolf
-  \___/|_|  |_|___\____/_/   \_\    Date: 2023/9/24
+  \___/|_|  |_|___\____/_/   \_\    Date: 2023/10/27
 
     ''')
     # Don't open debug mode because of duckdb database.
